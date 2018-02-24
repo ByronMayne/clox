6a03081fa62d3d1e856419f4497bd25e3a937153
ByronMayne <ByronMayne@gmail.com>
2018-02-24 12:20:42 -0500

    Disassembling Chunks [14.4]

---
+++ debug.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "debug.h"
+
+void disassembleChunk(Chunk * chunk, const char * name)
+{
+	printf("== %s ==\n", name);
+	for (int i = 0; i < chunk->count;) 
+	{
+		i = disassembleInstruction(chunk, i);
+	}
+}
+
+int disassembleInstruction(Chunk * chunk, int offset)
+{
+	printf("%04d ", offset);
+
+	uint8_t instruction = chunk->code[offset];
+	switch (instruction) 
+	{
+	case OP_RETURN:
+		return simpleInstruction("OP_RETURN", offset);
+	default:
+		printf("Unknown opcode %d\n", instruction);
+		return offset + 1;
+	}
+}
+
+static int simpleInstruction(const char* name, int offset)
+{
+	printf("%s\n", name);
+	return offset + 1;
+}
--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* alignas[])
 {
+	printf("Start");
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
+
+	getchar();
 }
