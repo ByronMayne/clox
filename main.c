408605787ea5784d601b194cb2885ddabf72798f
ByronMayne <ByronMayne@gmail.com>
2018-02-24 13:06:40 -0500

    Disassembling line information [14.6.1]

--- main.c
+++ main.c
@@ -9,10 +9,10 @@ int main(int argc, const char* alignas[])
     initChunk(&chunk);
 
 	int constant = addConstant(&chunk, 1.2);
-	writeChunk(&chunk, OP_CONSTANT);
-	writeChunk(&chunk, constant); 
+	writeChunk(&chunk, OP_CONSTANT, 123);
+	writeChunk(&chunk, constant, 123); 
 
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_RETURN, 123);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
 
