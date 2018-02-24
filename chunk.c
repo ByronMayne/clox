30cc5e559870999d8f2bdb488386ebe10f160bd7
ByronMayne <ByronMayne@gmail.com>
2018-02-24 12:38:20 -0500

    Value arrays [14.5.2]

--- chunk.c
+++ chunk.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include "chunk.h"
 #include "memory.h"
+#include "value.h"
 
 void initChunk(Chunk* chunk)
 {
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+	initValueArray(&chunk->constants);
 }
 
 void freeChunk(Chunk* chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+	freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
 
@@ -25,4 +28,10 @@ void writeChunk(Chunk* chunk, uint8_t byte)
     }
     chunk->code[chunk->count] = byte;
     chunk->count++;
+}
+
+int addConstant(Chunk * chunk, Value value)
+{
+	writeValueArray(&chunk->constants, value); 
+	return chunk->constants.count - 1;
 }
--- value.h
+++ value.h
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef clox_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+	int capacity;
+	int count;
+	Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+
+#endif // !clox_value_h
