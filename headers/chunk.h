fabef67f4c49cc882287b015fc692b3a047cec45
ByronMayne <ByronMayne@gmail.com>
2018-02-24 11:36:38 -0500

    Dynamic array of instructions [14.3.1]

--- headers/chunk.h
+++ headers/chunk.h
@@ -7,4 +7,14 @@ typedef enum {
     OP_RETURN,
 } OpCode;
 
+typedef struct {
+    int count;
+    int capacity;
+    uint8_t* code;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte);
+
 #endif
