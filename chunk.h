586808a7e1eea1bb412abfe3f008e82735295592
ByronMayne <ByronMayne@gmail.com>
2018-04-07 12:36:36 -0400

    15 . 3 . 1 Binary operators

--- chunk.h
+++ chunk.h
@@ -6,6 +6,10 @@
 
 typedef enum {
 	OP_CONSTANT,
+	OP_ADD,
+	OP_SUBTRACT,
+	OP_MULTIPLY,
+	OP_DIVIDE, 
 	OP_RETURN,
 	OP_NEGATE,
 } OpCode;
--- vm.c
+++ vm.c
@@ -15,6 +15,13 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+	do { \
+	  double b = pop(); \
+	  double a = pop(); \
+	  push(a op b); \
+	} while (false)
+
 	for (;;)
 	{
 #ifdef DEBUG_TRACE_EXECUTION
@@ -31,6 +38,10 @@ static InterpretResult run()
 		uint8_t instruction;
 		switch (instruction = READ_BYTE())
 		{
+			case OP_ADD:       BINARY_OP(+); break; 
+			case OP_SUBTRACT:  BINARY_OP(-); break;
+			case OP_MULTIPLY:  BINARY_OP(*); break;
+			case OP_DIVIDE:    BINARY_OP(/); break;
 			case OP_NEGATE:
 			{
 				push(-pop());
@@ -53,6 +64,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 void initVM()
