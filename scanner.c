70f93f1eafd67b9c0628a77a0aaef0799e9db22d
ByronMayne <Byronmayne@gmail.com>
2018-07-08 16:00:52 -0400

    16 . 3 A Lexical Grammar for Lox

--- scanner.c
+++ scanner.c
@@ -24,7 +24,22 @@ static bool isAtEnd()
 	return *scanner.current == '\0';
 }
 
-static Token MakeToken(TokenType tokenType)
+static char advance() 
+{
+	scanner.current++;
+	return scanner.current[-1];
+}
+
+static bool match(char expected)
+{
+	if (isAtEnd()) return false;
+	if (*scanner.current != expected) return false;
+
+	scanner.current++;
+	return true;
+}
+
+static Token makeToken(TokenType tokenType)
 {
 	Token token;
 	token.type = tokenType; 
@@ -48,7 +63,32 @@ Token scanToken()
 {
 	scanner.start = scanner.current;
 
-	if (isAtEnd()) return MakeToken(TOKEN_EOF);
+	if (isAtEnd()) return makeToken(TOKEN_EOF);
+
+	char c = advance();
+
+	switch (c)
+	{
+		case '(': return makeToken(TOKEN_LEFT_PAREN);
+		case ')': return makeToken(TOKEN_RIGHT_PAREN);
+		case '{': return makeToken(TOKEN_LEFT_BRACE);
+		case '}': return makeToken(TOKEN_RIGHT_BRACE);
+		case ';': return makeToken(TOKEN_SEMICOLON);
+		case ',': return makeToken(TOKEN_COMMA);
+		case '.': return makeToken(TOKEN_DOT);
+		case '-': return makeToken(TOKEN_MINUS);
+		case '+': return makeToken(TOKEN_PLUS);
+		case '/': return makeToken(TOKEN_SLASH);
+		case '*': return makeToken(TOKEN_STAR);
+		case '!':
+			return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+		case '=':
+			return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+		case '<':
+			return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+		case '>':
+			return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+	}
 
 	return errorToken("Unexpected character.");
 }
