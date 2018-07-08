0096c15f96ac11e612ada76dc160eccb1ac4ec12
ByronMayne <Byronmayne@gmail.com>
2018-07-08 16:12:38 -0400

    16 . 3 . 1 Whitespace

--- scanner.c
+++ scanner.c
@@ -24,10 +24,20 @@ static bool isAtEnd()
 	return *scanner.current == '\0';
 }
 
-static char advance() 
+static char advance()
 {
 	scanner.current++;
 	return scanner.current[-1];
+}
+
+static char peek()
+{
+	return *scanner.current;
+}
+
+static char peekNext() {
+	if (isAtEnd()) return '\0';
+	return scanner.current[1];
 }
 
 static bool match(char expected)
@@ -39,10 +49,45 @@ static bool match(char expected)
 	return true;
 }
 
+static void skipWhiteSpace()
+{
+	for (;;)
+	{
+		char c = peek();
+		switch (c)
+		{
+		case ' ':
+		case '\r':
+		case '\t':
+			advance();
+			break;
+		case '\n':
+			scanner.line++;
+			advance();
+			break;
+		case '/':
+			if (peekNext() == '/')
+			{
+				while (peek() != '\n' && !isAtEnd())
+				{
+					advance();
+				}
+			}
+			else
+			{
+				return;
+			}
+			break;
+		default:
+			return;
+		}
+	}
+}
+
 static Token makeToken(TokenType tokenType)
 {
 	Token token;
-	token.type = tokenType; 
+	token.type = tokenType;
 	token.start = scanner.start;
 	token.length = (int)(scanner.current - scanner.start);
 	token.line = scanner.line;
@@ -53,7 +98,7 @@ static Token errorToken(const char* message)
 {
 	Token token;
 	token.type = TOKEN_ERROR;
-	token.start = message; 
+	token.start = message;
 	token.length = (int)strlen(message);
 	token.line = scanner.line;
 	return token;
@@ -61,6 +106,8 @@ static Token errorToken(const char* message)
 
 Token scanToken()
 {
+	skipWhiteSpace();
+
 	scanner.start = scanner.current;
 
 	if (isAtEnd()) return makeToken(TOKEN_EOF);
@@ -69,25 +116,25 @@ Token scanToken()
 
 	switch (c)
 	{
-		case '(': return makeToken(TOKEN_LEFT_PAREN);
-		case ')': return makeToken(TOKEN_RIGHT_PAREN);
-		case '{': return makeToken(TOKEN_LEFT_BRACE);
-		case '}': return makeToken(TOKEN_RIGHT_BRACE);
-		case ';': return makeToken(TOKEN_SEMICOLON);
-		case ',': return makeToken(TOKEN_COMMA);
-		case '.': return makeToken(TOKEN_DOT);
-		case '-': return makeToken(TOKEN_MINUS);
-		case '+': return makeToken(TOKEN_PLUS);
-		case '/': return makeToken(TOKEN_SLASH);
-		case '*': return makeToken(TOKEN_STAR);
-		case '!':
-			return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
-		case '=':
-			return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
-		case '<':
-			return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
-		case '>':
-			return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
+	case '(': return makeToken(TOKEN_LEFT_PAREN);
+	case ')': return makeToken(TOKEN_RIGHT_PAREN);
+	case '{': return makeToken(TOKEN_LEFT_BRACE);
+	case '}': return makeToken(TOKEN_RIGHT_BRACE);
+	case ';': return makeToken(TOKEN_SEMICOLON);
+	case ',': return makeToken(TOKEN_COMMA);
+	case '.': return makeToken(TOKEN_DOT);
+	case '-': return makeToken(TOKEN_MINUS);
+	case '+': return makeToken(TOKEN_PLUS);
+	case '/': return makeToken(TOKEN_SLASH);
+	case '*': return makeToken(TOKEN_STAR);
+	case '!':
+		return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
+	case '=':
+		return makeToken(match('=') ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL);
+	case '<':
+		return makeToken(match('=') ? TOKEN_LESS_EQUAL : TOKEN_LESS);
+	case '>':
+		return makeToken(match('=') ? TOKEN_GREATER_EQUAL : TOKEN_GREATER);
 	}
 
 	return errorToken("Unexpected character.");
