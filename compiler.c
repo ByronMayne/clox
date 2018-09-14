bd49c3db2b0989a0f4bafe551f319726282c2d07
ByronMayne <Byronmayne@gmail.com>
2018-09-14 19:27:58 -0400

    17 . 2 Parsing Tokens

--- compiler.c
+++ compiler.c
@@ -3,10 +3,29 @@
 #include "compiler.h"
 #include "scanner.h";
 
+typedef struct
+{
+	Token current;
+	Token previous;
+} Parser;
+
+Parser parser;
+
 bool compile(const char* source, Chunk* chunk)
 {
 	initScanner(source);
 	advance();
 	expression();
 	consume(TOKEN_EOF, "Expected end of session");
+}
+
+static void advance() {
+	parser.previous = parser.current;
+
+	for (;;) {
+		parser.current = scanToken();
+		if (parser.current.type != TOKEN_ERROR) break;
+
+		errorAtCurrent(parser.current.start);
+	}
 }
