f5483234f96d0b0f201f79d381eee820371dee4a
ByronMayne <Byronmayne@gmail.com>
2018-09-16 17:44:27 -0400

    17 . 7 Dumping Chunks

--- compiler.c
+++ compiler.c
@@ -4,6 +4,10 @@
 #include "compiler.h"
 #include "scanner.h";
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"     
+#endif    
+
 typedef struct
 {
 	Token current;
@@ -130,6 +134,13 @@ static void emitConstant(Value value)
 static void endCompiler()
 {
 	emitReturn();
+#ifdef  DEBUG_PRINT_CODE
+	if (!parser.hadError) 
+	{
+		disassembleChunk(currentChunk(), "code");
+	}
+#endif //  DEBUG_PRINT_CODE
+
 }
 
 static void expression();
