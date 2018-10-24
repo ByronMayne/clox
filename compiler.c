55299e4ff3b018465c5b3b521de6d4a0e1dafefb
ByronMayne <Byronmayne@gmail.com>
2018-10-24 17:02:39 -0400

    18 . 4 Two New Types

--- compiler.c
+++ compiler.c
@@ -186,6 +186,18 @@ static void binary()
 	}
 }
 
+static void literal()
+{
+	switch (parser.previous.type)
+	{
+		case TOKEN_TRUE: emitByte(OP_TRUE); break; 
+		case TOKEN_FALSE: emitByte(OP_FALSE); break; 
+		case TOKEN_NIL: emitByte(OP_NIL); break;
+		default:
+			return; // Unreachable. 
+	}
+}
+
 static void grouping()
 {
 	expression();
@@ -238,17 +250,17 @@ ParseRule rules[] = {
 	{ NULL,     NULL,    PREC_AND },        // TOKEN_AND             
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_CLASS           
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_ELSE            
-	{ NULL,     NULL,    PREC_NONE },       // TOKEN_FALSE           
+	{ literal,  NULL,    PREC_NONE },       // TOKEN_FALSE           
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_FUN             
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_FOR             
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_IF              
-	{ NULL,     NULL,    PREC_NONE },       // TOKEN_NIL             
+	{ literal,  NULL,    PREC_NONE },       // TOKEN_NIL             
 	{ NULL,     NULL,    PREC_OR },         // TOKEN_OR              
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_PRINT           
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_RETURN          
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_SUPER           
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_THIS            
-	{ NULL,     NULL,    PREC_NONE },       // TOKEN_TRUE            
+	{ literal,  NULL,    PREC_NONE },       // TOKEN_TRUE            
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_VAR             
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_WHILE           
 	{ NULL,     NULL,    PREC_NONE },       // TOKEN_ERROR           
--- scanner.c
+++ scanner.c
@@ -131,7 +131,7 @@ static TokenType checkKeyword(int start, int length, const char* rest, TokenType
 
 static TokenType identifierType()
 {
-	switch (scanner.current[0])
+	switch (scanner.start[0])
 	{
 	case 'a': return checkKeyword(1, 2, "nd", TOKEN_AND);
 	case 'c': return checkKeyword(1, 4, "lass", TOKEN_CLASS);
